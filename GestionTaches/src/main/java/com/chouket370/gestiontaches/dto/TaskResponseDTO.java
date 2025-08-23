03b6577c3e7b71de74c86f2eaf7eac42c1f39bdb
Mohamed Yassine Chouket <181577092+yassinechouket@users.noreply.github.com>
2025-08-23 15:36:42 +0100

    Update TaskResponseDTO.java

--- GestionTaches/src/main/java/com/chouket370/gestiontaches/dto/TaskResponseDTO.java
+++ GestionTaches/src/main/java/com/chouket370/gestiontaches/dto/TaskResponseDTO.java
@@ -1,5 +1,6 @@
 package com.chouket370.gestiontaches.dto;
 
+import com.chouket370.gestiontaches.model.Priority;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,16 +13,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class TaskResponseDTO {
-    private Long id;
+    private Long taskId;
+    private Long ownerId;
+    private Long assignedId;
     private String title;
     private String description;
-    private LocalDateTime dueDate;
-    private boolean completed;
-    private boolean archived;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private String assignedToUsername;
     private Priority priority;
-
+    private LocalDateTime dueTime;
+    private LocalDateTime lastUpdate;
 }
-
