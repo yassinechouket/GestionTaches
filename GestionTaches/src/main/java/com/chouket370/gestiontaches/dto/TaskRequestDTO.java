725fcb58acd1a4641ba1cdb29785548f3fca72e6
Mohamed Yassine Chouket <181577092+yassinechouket@users.noreply.github.com>
2025-08-23 15:36:22 +0100

    Update TaskRequestDTO.java

--- GestionTaches/src/main/java/com/chouket370/gestiontaches/dto/TaskRequestDTO.java
+++ GestionTaches/src/main/java/com/chouket370/gestiontaches/dto/TaskRequestDTO.java
@@ -1,5 +1,6 @@
 package com.chouket370.gestiontaches.dto;
 
+import com.chouket370.gestiontaches.model.Priority;
 import jakarta.validation.constraints.FutureOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -15,11 +17,7 @@ import java.time.LocalDateTime;
 public class TaskRequestDTO {
     private String title;
     private String description;
-    @FutureOrPresent(message = "Due date must be in the future")
     private LocalDateTime dueDate;
-    private boolean completed;
-    private boolean archived;
-    private String assignedToUsername;
     private Priority priority;
+    private Long assigneeId;
 }
-
